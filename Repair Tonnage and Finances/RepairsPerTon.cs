7f01f7d7b9701fa67626cdc053d3a34026483b7b
Bradley Burcar <btburcar@gmail.com>
2019-11-09 23:06:22 -0500

    Logging for bug.

--- Repair Tonnage and Finances/RepairsPerTon.cs
+++ Repair Tonnage and Finances/RepairsPerTon.cs
@@ -19,6 +19,8 @@ namespace Repair_Tonnage
                     RPTData.lastrepairstate = __instance;
                     foreach (MechDef mechDef in __instance.ActiveMechs.Values)
                     {
+                        Logger.Log(mechDef.GUID);
+                        Logger.Log(mechSimGameUID);
                         if (mechDef.GUID == mechSimGameUID)
                         {
                             float num = mechDef.Chassis.Tonnage;
